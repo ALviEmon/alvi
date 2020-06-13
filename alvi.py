4077a0e97f17b6b3dce216d6f1755c82484f46ec
ALviEmon <66849019+ALviEmon@users.noreply.github.com>
2020-06-13 10:26:37 +0600

    Create alvi.py

---
+++ alvi.py
@@ -0,0 +1,20 @@
+# I'm One In Billion.
+
+
+
+
+* pkg update
+
+* pkg upgrade
+
+* pkg install python
+
+* pkg install python2
+
+* pip2 install requests mechanize
+
+* git clone https://github.com/ALviEmon/alvi
+
+* cd alvi
+
+* python2 alvi.py
